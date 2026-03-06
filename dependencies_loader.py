79e9a4a674745345d0e88e5a5800233c1a40a9da
Dominik Ślusarz <slusarz.dominik@gmail.com>
2026-03-06 18:29:19 +0100

    prevent loading same file twice

--- dependencies_loader.py
+++ dependencies_loader.py
@@ -50,7 +50,7 @@ class Loader:
     def _prepare_imported_dependencies(self, file: File, dependency: Dependency) -> list[Dependency]:
         file_spawn = file.spawn(dependency.prepare_path())
         properties = load_properties(file_spawn)
-        return self.load_dependencies(properties, file_spawn)
+        return self._load_dependencies(file_spawn, properties)
 
     def _get_dependencies(self, dependencies_source: dict):
         dependency = dependencies_source['dependency']
